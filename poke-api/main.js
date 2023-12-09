6f3db5e8865478d13b6a341b6105afdbea16cf53
Daniel An <cruzalexd1@gmail.com>
2023-12-08 20:48:55 -0600

    added pokeball image to cards

--- poke-api/main.js
+++ poke-api/main.js
@@ -3,29 +3,35 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 document.querySelector('#app').innerHTML = `
 
  <section>
-<div class="container">
-  <form id="searchForm" class="my-4">
-    <div class="form-group"> <label for="searchInput">Buscar personaje:</label> <input type="text"
-        class="form-control" id="pokemon-search" placeholder="Ingrese el nombre del personaje"> </div> <button
-      type="submit" class="btn btn-primary">Buscar</button>
-  </form>
-  <div class="row" id="pokemon-card"></div>
-</div>
-</section> 
+       <div class="container">
+        <form id="searchForm" class="my-4">
+           <div class="form-group"> 
+             <label for="searchInput">Buscar personaje:</label> 
+             <input type="text" class="form-control" id="pokemonsearch" placeholder="Ingrese el nombre del personaje"> 
+             </div> 
+             <button type="submit" class="btn btn-primary">Buscar</button>
+         </form>
+       <div class="row" id="pokemon-card"></div>
+      </div>
+  </section> 
 `
 
 const getCharacters = async (contenidoInput = '') => {
-/*   let contenidoInput = document.getElementById("pokemon-name").value;
- */  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${contenidoInput}`)
+
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${contenidoInput}`)
   const data = await response.json()
-  console.log(data);
+  console.log(data.results);
+
+  const pokemonsCardContainer = document.querySelector('#pokemon-card')
+  pokemonsCardContainer.innerHTML = ''
 
   data.results.map((char) => {
     const pokemonCard = document.createElement('div')
+    pokemonCard.classList.add('col-sm-12', 'col-md-4')
 
     pokemonCard.innerHTML = `
     <div class="card" style="width: 18rem;">
-    <img src="${char.url}" class="card-img-top" alt="">
+    <img src="https://res.cloudinary.com/doi7ukubm/image/upload/f_auto,q_auto/frqhrdp5vycaa7fux6gi" class="card-img-top" alt="">
     <div class="card-body">
       <h5 class="card-title">${char.name}</h5>
       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
@@ -34,14 +40,13 @@ const getCharacters = async (contenidoInput = '') => {
     </div>
   </div>
     `
-
-    document.querySelector('#pokemon-card').appendChild(pokemonCard)
-  })
+    pokemonsCardContainer.appendChild(pokemonCard)
+ })
 }
 
-document.getElementById('searchForm').addEventListener('click', (event) => {
+document.getElementById('searchForm').addEventListener('submit', (event) => {
   event.preventDefault()
-  const search = document.getElementById('pokemon-search').value
+  const search = document.getElementById('pokemonsearch').value
   getCharacters(search)
 })
 
