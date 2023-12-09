535239178a376c0e7e3e3caa4f059011bc5c3f60
Edgar Fernando Lopez Zavaleta <90074636+FerNIX1@users.noreply.github.com>
2023-12-08 21:27:04 -0600

    funcionalidad del nombre

--- poke-api/main.js
+++ poke-api/main.js
@@ -1,55 +1,56 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
+// main.js
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 document.querySelector('#app').innerHTML = `
-
- <section>
-       <div class="container">
-        <form id="searchForm" class="my-4">
-           <div class="form-group"> 
-             <label for="searchInput">Buscar personaje:</label> 
-             <input type="text" class="form-control" id="pokemonsearch" placeholder="Ingrese el nombre del personaje"> 
-             </div> 
-             <button type="submit" class="btn btn-primary">Buscar</button>
-         </form>
-       <div class="row" id="pokemon-card"></div>
+<section>
+  <div class="container">
+    <form id="searchForm" class="my-4">
+      <div class="form-group">
+        <label for="pokemonSearchInput">Buscar personaje:</label>
+        <input type="text" class="form-control" id="pokemonSearchInput" placeholder="Ingrese el nombre del personaje">
       </div>
-  </section> 
-`
+      <button type="submit" class="btn btn-primary">Buscar</button>
+    </form>
+    <div class="row" id="pokemon-card"></div>
+  </div>
+</section>
+`;
 
-const getCharacters = async (contenidoInput = '') => {
+const getCharacters = async (searchTerm = "") => {
+  searchTerm.toLowerCase(); // Esto no modifica searchTerm, deberías asignarlo de nuevo
 
-  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${contenidoInput}`)
-  const data = await response.json()
-  console.log(data.results);
+  try {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${searchTerm}`);
+    const data = await response.json();
+    console.log(data);
 
-  const pokemonsCardContainer = document.querySelector('#pokemon-card')
-  pokemonsCardContainer.innerHTML = ''
+    const pokemonsCardContainer = document.querySelector('#pokemon-card');
+    pokemonsCardContainer.innerHTML = '';
 
-  data.results.map((char) => {
-    const pokemonCard = document.createElement('div')
-    pokemonCard.classList.add('col-sm-12', 'col-md-4')
+    const pokemonCard = document.createElement('div');
+    pokemonCard.classList.add('col-sm-12', 'col-md-4');
 
     pokemonCard.innerHTML = `
-    <div class="card" style="width: 18rem;">
-    <img src="https://res.cloudinary.com/doi7ukubm/image/upload/f_auto,q_auto/frqhrdp5vycaa7fux6gi" class="card-img-top" alt="">
-    <div class="card-body">
-      <h5 class="card-title">${char.name}</h5>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-        content.</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
-    </div>
-  </div>
-    `
-    pokemonsCardContainer.appendChild(pokemonCard)
- })
-}
+      <div class="card" style="width: 18rem;">
+        <img src="https://res.cloudinary.com/doi7ukubm/image/upload/f_auto,q_auto/frqhrdp5vycaa7fux6gi" class="card-img-top" alt="">
+        <div class="card-body">
+          <h5 class="card-title">${data.name}</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    `;
 
-document.getElementById('searchForm').addEventListener('submit', (event) => {
-  event.preventDefault()
-  const search = document.getElementById('pokemonsearch').value
-  getCharacters(search)
-})
+    pokemonsCardContainer.appendChild(pokemonCard);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
 
-getCharacters()
+document.getElementById('searchForm').addEventListener('submit', function (event) {
+  event.preventDefault();
+  const searchTerm = document.getElementById('pokemonSearchInput').value;
+  getCharacters(searchTerm);
+});
 
-
+getCharacters(); // Cargar personajes al inicio
