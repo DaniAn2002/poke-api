df11f041588ad3e33456eb7c8b08b0842330f1a2
erbiti <tommy.ponce@gmail.com>
2023-12-11 11:33:36 -0600

    feat(ejemplos) algunas modificacion en el main.js

--- poke-api/main.js
+++ poke-api/main.js
@@ -9,9 +9,20 @@ document.querySelector('#app').innerHTML = `
 `
 
 const getCharacters = async () =>{
-  const response = await fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+  const response = await fetch('https://pokeapi.co/api/v2/pokemon')
   const data = await response.json()
-  console.log(data);
+   data.results.map((character,index) =>{
+      if (character.name === 'charmander')
+        console.log(index)
+
+   })
+  
+
+
+  console.log(data.results);
 }
+
+
+
 
 getCharacters()
